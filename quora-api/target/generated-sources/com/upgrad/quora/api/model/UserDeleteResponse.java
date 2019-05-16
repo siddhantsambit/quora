277ed8960df57cb91402c04146169689ce65a8f4
siddhantsambit <35418098+siddhantsambit@users.noreply.github.com>
2019-05-16 12:37:06 +0530

    Answer Controller (#9)
    Mergin using administrator privilege

--- quora-api/target/generated-sources/com/upgrad/quora/api/model/UserDeleteResponse.java
+++ quora-api/target/generated-sources/com/upgrad/quora/api/model/UserDeleteResponse.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.*;
  * UserDeleteResponse
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-05-08T16:11:02.467+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-05-16T12:32:12.934+05:30")
 
 public class UserDeleteResponse   {
   @JsonProperty("id")
